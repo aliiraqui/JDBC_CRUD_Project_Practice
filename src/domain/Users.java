82c977b9ea17a8d7e8a59c1e64ddf16d5f3c4d58
Ali Ahmed Iraqui <aliiraqui@hotmail.com>
2024-08-15 08:05:05 +0530

    Created Users Class

---
+++ src/domain/Users.java
@@ -0,0 +1,61 @@
+package domain;
+
+import java.util.Objects;
+
+public class Users {
+    private int id;
+    private String name;
+    private String email;
+
+    public Users(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return getId() == users.getId() && Objects.equals(getName(), users.getName()) && Objects.equals(getEmail(), users.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getEmail());
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
